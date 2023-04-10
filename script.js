875e97ad8b5b11de144cba4486cd549cf54542be
ULISES ANIBAL BAAMONDE <45687573@ort>
2023-04-10 15:58:18 -0300

    Tachado casi completado

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 var arrayTareas = []
 function Listar(event){
     event.preventDefault();
+    var Tarea = new Object()
+   
     let valorImput = document.getElementById("inputt").value
-
+    
     if(validarInput(valorImput))
     {
+        Tarea.completa = false
+        Tarea.inicio = event.timeStamp
+        Tarea.texto= valorImput;
+        Tarea.fin = -1;
         let lista = document.getElementById("TODOlista")
         lista.appendChild(CrearElemendo(valorImput))
-
+        arrayTareas.push(Tarea)
     }
 }
 let validarInput = valorImput => {
@@ -29,6 +35,7 @@ let CrearElemendo = valorImput =>{
     let c = document.createElement("label")
     c.className = "form-check-label"
     c.innerHTML += valorImput
+    c.className = "destachado"
     a.appendChild(c)
     let d = document.createElement("hr")
     a.appendChild(d)
@@ -40,12 +47,39 @@ let Tachar = event =>{
     let Padre = current.parentNode;
     let hijo = Padre.lastChild
     hijo = hijo.previousElementSibling
-    if(hijo.style.textDecoration == "line-through")
-    {
+    for (let i = 0; i < arrayTareas.length; i++) {
+        if(arrayTareas[i].texto = hijo.innerHTML){
+            if(arrayTareas[i].completa == false)
+            {
+                hijo.classList.remove("destachado");
+                hijo.className = "tachado";
+                arrayTareas[i].completa = true
+                arrayTareas[i].fin = event.timeStamp
+                
+            }
+            else if(arrayTareas[i].completa == true)
+            {
+                hijo.classList.remove("tachado");
+                hijo.className = "destachado";
+                arrayTareas[i].completa = false
+                arrayTareas[i].fin = -1
+            }
+            console.log(arrayTareas[i])
+        }
         
-        hijo.className = "text-decoration-none";
+        
     }
-    else{
-    hijo.style.textDecoration = "line-through";
+}
+
+let BuscarMasRapida = event =>{
+    event.preventDefault();
+    let arrayRapidas = []
+    let numMayor = 99999999999
+    for (let i = 0; i < arrayTareas.length; i++) {
+        if (arrayTareas[i].fin - arrayTareas[i].inicio < numMayor) {
+            arrayRapidas.push(arrayTareas[i])
+            alert("La tarea es mas rapida es " + arrayRapidas[i].texto)
+        }
     }
+    
 }
